96aeedae0327a988b34f8063ec51eaa35559c215
icqcq39 <cute_cloudian@hotmail.com>
2016-11-03 00:32:28 +0700

    before sleep
    +PlayScreen

--- desktop/src/com/cpe/magigo/desktop/DesktopLauncher.java
+++ desktop/src/com/cpe/magigo/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.cpe.magigo.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.cpe.magigo.magiGO;
+import com.cpe.magigo.MagiGO;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new magiGO(), config);
+		new LwjglApplication(new MagiGO(), config);
 	}
 }
