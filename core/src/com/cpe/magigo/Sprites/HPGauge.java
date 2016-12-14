8c7495f81323596d4295ab46d99f36ee351efd80
Toufu <timecrisis1995@live.com>
2016-12-14 14:42:19 +0700

    Fix HP bar didn't update

--- core/src/com/cpe/magigo/Sprites/HPGauge.java
+++ core/src/com/cpe/magigo/Sprites/HPGauge.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.cpe.magigo.MagiGO;
 import com.cpe.magigo.Screens.PlayScreen;
+import com.cpe.magigo.Sprites.Magic.Magic;
 
 public class HPGauge extends Sprite {
     private Sprite HP_FG ;
@@ -16,23 +17,25 @@ public class HPGauge extends Sprite {
     private Sprite HP_BG;
     private Texture BG = new Texture("HealtBar/source/health_bar.png");
     private TextureRegion BG_R;
-    private int Hp;
-    public HPGauge(int hp)
+    private int Hp=100;
+    public HPGauge()
     {
-        this.Hp = hp;
+       // this.Hp = hp;
         BG_R = new TextureRegion(BG);
         FG_R = new TextureRegion(FG);
-        HP_FG = new Sprite(FG_R,0,0,500,83);
+        HP_FG = new Sprite(FG_R,0,0,FG_R.getRegionWidth(),FG_R.getRegionHeight());
         HP_BG = new Sprite(BG_R);
         HP_BG.setOrigin(0,0);
         HP_FG.setOrigin(0,0);
     }
 
-    public void update()
+    public void update(float dt)
     {
         HP_BG.setPosition((MagiGO.V_WIDTH/2-270)/MagiGO.PPM, 100 / MagiGO.PPM);
         HP_FG.setPosition((MagiGO.V_WIDTH/2-270)/MagiGO.PPM, 100 / MagiGO.PPM);
         HP_BG.setScale(0.65f/MagiGO.PPM,0.5f/MagiGO.PPM);
+        HP_FG.setSize(this.Hp*(FG_R.getRegionWidth()/100),FG_R.getRegionHeight());
+        HP_FG.setRegion(0,0,this.Hp*(FG_R.getRegionWidth()/100),FG_R.getRegionHeight());
         HP_FG.setScale(0.65f/MagiGO.PPM,0.5f/MagiGO.PPM);
     }
 
@@ -46,5 +49,10 @@ public class HPGauge extends Sprite {
     {
         HP_BG.draw(batch);
         //HP_FG.draw(batch);
+    }
+
+    public void hit()
+    {
+        Hp-=10;
     }
 }
