49e4912fe273a136ecf6580b4f9cc2a25a7bb49d
chalitphol kuensuwan <chalitphol@gmail.com>
2016-12-15 13:57:10 +0700

    final

--- core/src/com/cpe/magigo/Screens/PlayScreen.java
+++ core/src/com/cpe/magigo/Screens/PlayScreen.java
@@ -277,7 +277,7 @@ public class PlayScreen implements Screen {
         renderer.render();
 
         //renderer our Box2DDebugLines
-        b2dr.render(world, gamecam.combined);
+        //b2dr.render(world, gamecam.combined);
 
         game.batch.setProjectionMatrix(gamecam.combined);
         //game.batch.draw(Hp,0,0,10,5);
